57e50cf48732022ab2c313b1a991093771c69592
Ewoud Van Vooren <44306474+EwoudVV@users.noreply.github.com>
2025-03-27 19:58:24 -0400

    yeeg

--- bot.py
+++ bot.py
@@ -6,30 +6,29 @@ from dotenv import load_dotenv
 load_dotenv()
 logging.basicConfig(level=logging.DEBUG)
 
-slack_bot_token = os.environ["SLACK_BOT_TOKEN"]
-slack_signing_secret = os.environ["SLACK_SIGNING_SECRET"]
-app = App(token=slack_bot_token, signing_secret=slack_signing_secret)
-
-TARGET_CHANNEL = "C085QFM80G7" #ewouds-engineering
+app = App(token=os.environ["SLACK_BOT_TOKEN"], signing_secret=os.environ["SLACK_SIGNING_SECRET"])
 
 @app.event("message")
 def handle_message_event(body, logger, event, client):
     try:
         logger.info(f"Incoming event: {event}")
         
-        if event.get('subtype') is None and event.get('text', '').strip().lower() == 'bbb':
+        if event.get('subtype') is None and event.get('text', '').strip().lower() == 'work pls':
             logger.info("Trigger condition met")
             
-            client.conversations_join(channel=TARGET_CHANNEL)
-            
             response = client.chat_postMessage(
-                channel=TARGET_CHANNEL,
+                channel=event['channel'],
                 text="first message sent! :yay:"
             )
             logger.info(f"Message sent: {response}")
             
     except Exception as e:
         logger.error(f"Error: {e}")
+
+@app.event("app_mention")
+def handle_mention(event, say):
+    say("this better work")
+
 
 if __name__ == "__main__":
     app.start(port=int(os.environ.get("PORT", 3000)))
