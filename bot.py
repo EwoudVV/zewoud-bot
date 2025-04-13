5ca0c1f07fd0bc1ac90b254ea87e8352709ff2fd
Ewoud Van Vooren <44306474+EwoudVV@users.noreply.github.com>
2025-04-13 13:21:03 -0400

    e

--- bot.py
+++ bot.py
@@ -8,27 +8,24 @@ logging.basicConfig(level=logging.DEBUG)
 
 app = App(token=os.environ["SLACK_BOT_TOKEN"], signing_secret=os.environ["SLACK_SIGNING_SECRET"])
 
-@app.event("message")
-def handle_message_event(body, logger, event, client):
-    try:
-        logger.info(f"Incoming event: {event}")
-        
-        if event.get('subtype') is None and event.get('text', '').strip().lower() == 'work pls':
-            logger.info("Trigger condition met")
-            
-            response = client.chat_postMessage(
-                channel=event['channel'],
-                text="first message sent! :yay:"
-            )
-            logger.info(f"Message sent: {response}")
-            
-    except Exception as e:
-        logger.error(f"Error: {e}")
+@app.middleware
+def log_request(logger, body, next):
+    logger.info(f"REQUEST RECEIVED: {body}")
+    next()
 
 @app.event("app_mention")
-def handle_mention(event, say):
-    say("this better work")
+def handle_mention(event, say, logger):
+    logger.info(f"MENTION EVENT RECEIVED: {event}")
+    say("aaaaa")
 
+@app.event("message")
+def handle_message_event(body, logger, event, client):
+    logger.info(f"INCOMING MESSAGE EVENT: {event}")
+    if event.get('subtype') is None and event.get('text', '').strip().lower() == 'reee':
+        client.chat_postMessage(
+            channel=event['channel'],
+            text="reeee"
+        )
 
 if __name__ == "__main__":
     app.start(port=int(os.environ.get("PORT", 3000)))
